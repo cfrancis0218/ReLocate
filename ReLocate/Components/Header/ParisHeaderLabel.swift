c246e5de52d7816b40ed808f4c7cc868c8789329
Christian Francis <cfcoding@yahoo.com>
2020-10-20 15:41:35 -0230

    Label Added

---
+++ ReLocate/Components/Header/ParisHeaderLabel.swift
@@ -0,0 +1,20 @@
+//
+//  ParisHeaderLabel.swift
+//  ReLocate
+//
+//  Created by Christian Elijah on 2020-10-20.
+//
+
+import UIKit
+
+class ParisHeaderLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
